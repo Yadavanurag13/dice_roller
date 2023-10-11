80754e22831bf8069e04598b405ba4e153c9e8fd
Anurag Yadav <yadavanurag1310@gmail.com>
2023-10-11 10:14:14 +0530

    This is my third commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
 
-import 'package:dice_roller/style_text.dart';
+import 'package:dice_roller/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  // List in dart can be edited even it is final
+
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyleText(),
+      child: Center(
+        child: DiceRoller(),
       ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, this.colors});
+//
+//   final List<Color> colors;
+//   // List in dart can be edited even it is final
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyleText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
--- lib/style_text.dart
+++ lib/style_text.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class StyleText extends StatelessWidget {
-  const StyleText({super.key});
+  const StyleText(this.text, {super.key});
+
+  final String text;
 
   @override
-  Widget build(context){
-    return Text('Hello World!', style: TextStyle(
-      color: Colors.white,
-      fontSize: 30,
-    ),);
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 30,
+      ),
+    );
   }
 }
